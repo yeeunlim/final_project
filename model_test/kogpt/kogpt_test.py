3593cedaa4e18770f6b338013230fe261dd12c5d
jjunga1203 <156975193+jjunga1203@users.noreply.github.com>
2024-06-03 17:58:46 +0900

    kogpt pytorch로 수행

--- model_test/kogpt/kogpt_test.py
+++ model_test/kogpt/kogpt_test.py
@@ -1,31 +1,41 @@
-import tensorflow as tf
-from transformers import AutoTokenizer
-from transformers import TFGPT2LMHeadModel
+import torch
+from transformers import AutoTokenizer, GPT2LMHeadModel
 
 def chatbot(text):
     # input sentence : "질문" / 응답
     sentence = '<unused0>' + text + '<unused1>'
     tokenized = [tokenizer.bos_token_id] + tokenizer.encode(sentence)
-    tokenized = tf.convert_to_tensor([tokenized])
+    tokenized = torch.tensor([tokenized]).to(device)
 
     # 질문 문장으로 "레이블 + 응답" 토큰 생성
-    result = model.generate(tokenized, min_length = 15, max_length = 50, repetition_penalty = 1.3,
-                            do_sample = True, no_repeat_ngram_size = 5, temperature = 0.01,
-                            top_k = 2)
+    result = model.generate(tokenized, min_length=15, max_length=50, repetition_penalty=1.3,
+                            do_sample=True, no_repeat_ngram_size=5, temperature=0.01,
+                            top_k=2)
 
-    output = tokenizer.decode(result[0].numpy().tolist())
+    output = tokenizer.decode(result[0].tolist())
 
     response = output.split('<unused1> ')[1]
 
-    return  response
+    return response
 
 if __name__ == "__main__":
+    # GPU를 사용하려면 모델을 CUDA로 이동
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     save_model_path = "./kogpt_model/"
     model_name = 'song_emotion_chatbot'
-    tokenizer = AutoTokenizer.from_pretrained(save_model_path + model_name,  bos_token='</s>', eos_token='</s>', pad_token='<pad>')
-    model = TFGPT2LMHeadModel.from_pretrained(save_model_path + model_name)
+    tokenizer = AutoTokenizer.from_pretrained(save_model_path + model_name, bos_token='</s>', eos_token='</s>', pad_token='<pad>')
 
-    while 1:
+    # GPT2 모델 초기화
+    model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
+
+    # 모델의 가중치 로드
+    model.load_state_dict(torch.load(save_model_path + 'kogpt2_model.pth', map_location=device))
+    model.to(device)
+
+    print('Model loaded successfully')
+
+    while True:
         sent = input('\nQuestion: ')  # '요즘 기분이 우울한 느낌이에요'
         data = chatbot(sent)
         data = data.split('</s')
@@ -33,5 +43,6 @@ if __name__ == "__main__":
         if '종료' in sent:
             break
 
+        print(f'{sent}')
         print(f'response: {data[0]}')
         print('-' * 50)
--- model_test/kogpt/kogpt_train.py
+++ model_test/kogpt/kogpt_train.py
@@ -1,110 +1,73 @@
 import pandas as pd
 from tqdm import tqdm
-import tensorflow as tf
-from transformers import AutoTokenizer
-from transformers import TFGPT2LMHeadModel
+import torch
+from transformers import AutoTokenizer, GPT2LMHeadModel, AdamW
 import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import warnings
+warnings.filterwarnings('ignore')
 
 dir_path = './data/'
-sampling_cnt = 5 #분류별 가져올 샘플 개수
-EPOCHS = 1
-batch_size = 1
+EPOCHS = 3
+batch_size = 4
 
-# tokenizer/model  가져오기 (skt/kogpt2-base-v2)
+# tokenizer/model 가져오기 (skt/kogpt2-base-v2)
 tokenizer = AutoTokenizer.from_pretrained('skt/kogpt2-base-v2', bos_token='</s>', eos_token='</s>', pad_token='<pad>')
-model = TFGPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2', from_pt=True)
-
-def get_emotion():
-    emotion_df = pd.read_excel(dir_path + '감성대화말뭉치(val+tran_1대화)_답변수정.xlsx')
-
-    # label2 (category)별로 뽑아서 학습하기 위하여
-    chatbot_df = pd.DataFrame(columns=emotion_df.columns)  # 결과를 저장할 빈 데이터프레임 생성
-    for category, group in emotion_df.groupby('label2'):
-        selected_rows = group.sample(n=min(sampling_cnt, len(group)), replace=False)  # 각 그룹에서 최대 30개의 행을 랜덤하게 선택
-        chatbot_df = pd.concat([chatbot_df, selected_rows])  # 선택된 행을 결과 데이터프레임에 추가
-
-    return chatbot_df
-
-def get_song_emotion():
-    # 송영숙 데이터 챗봇에 추가하기
-    song_df = pd.read_excel(dir_path + '송영숙 챗봇 학습용 데이터.xlsx')
-    song_df = song_df.dropna()
-
-    # 라벨 없는 행 제거
-    song_df = song_df[(song_df['label']==0) | (song_df['label']==1) | (song_df['label']==2) ]
-
-    song = pd.DataFrame(columns=['label', 'Q', 'A']) 
-    for label, group in song_df.groupby('label'):
-        selected_rows = group.sample(n=min(sampling_cnt, len(group)), replace=False)  # 각 그룹에서 최대 30개의 행을 랜덤하게 선택
-        song = pd.concat([song, selected_rows])  # 선택된 행을 결과 데이터프레임에 추가
-    song.reset_index()
-    song.columns = ['label', 'question', 'response']
-
-    return song
-
-def get_merge_data():
-    df1 = get_emotion()  # 감성말뭉치 데이터
-    df2 = get_song_emotion() # 송영숙 데이터
-    
-    # 송영숙 챗봇 데이터와, 감성 말뭉치 합치기
-    df1 = df1[['question', 'response']]
-    df2 = df2[['question', 'response']]
-    song_emotion_df = pd.concat([df2, df1])
-    song_emotion_df.reset_index(inplace=True, drop=True)
-
-    return song_emotion_df
+model = GPT2LMHeadModel.from_pretrained('skt/kogpt2-base-v2')
 
 def tokenize_df():
-    chatbot_df = get_merge_data()
-
+    chatbot_df = pd.read_excel(dir_path + '감성대화말뭉치(train+val)_송영숙_싱글턴_완성.xlsx')
+    print(chatbot_df.shape)
     # 질문, 레이블 응답 문장을 불러옵니다.
-    for question, response in zip(chatbot_df.question.to_list(),  chatbot_df.response.to_list()):
+    for question, response in zip(chatbot_df.question.to_list(), chatbot_df.response.to_list()):
         # 문장의 BOS token : </s>
         bos_token = [tokenizer.bos_token_id]
         # 문장의 EOS token : </s>
         eos_token = [tokenizer.eos_token_id]
-        # print(question, response)
-        #문장 구조 : BOS + 질문 + (토큰으로 구분) + 응답 + EOS
+        # 문장 구조 : BOS + 질문 + (토큰으로 구분) + 응답 + EOS
         sentence = tokenizer.encode('<unused0>' + question + '<unused1>' + response)
 
         yield bos_token + sentence + eos_token
 
-def fit_model(dataset):
-    for epoch in tqdm(range(EPOCHS)):
+def collate_fn(batch):
+    # batch에서 가장 긴 문장을 기준으로 zero-padding을 진행합니다.
+    max_length = max(len(sample) for sample in batch)
+    batch_padded = [sample + [tokenizer.pad_token_id] * (max_length - len(sample)) for sample in batch]
+    return torch.tensor(batch_padded)
+
+def fit_model(dataset, optimizer):
+    model.train()
+    for epoch in range(EPOCHS):
         train_loss = 0
 
         for batch in tqdm(dataset):
-            try:
-                with tf.GradientTape() as tape:
-                    result = model(batch, labels=batch)
-                    loss = result[0]
-                    batch_loss = tf.reduce_mean(loss, -1)
-                    train_loss += batch_loss
+            inputs = batch.to(torch.device('cuda' if torch.cuda.is_available() else 'cpu'))
+            optimizer.zero_grad()
+            outputs = model(inputs, labels=inputs)
+            loss = outputs.loss
+            train_loss += loss.item()
+            loss.backward()
+            optimizer.step()
 
-                grads = tape.gradient(batch_loss, model.trainable_variables)
-                adam.apply_gradients(zip(grads, model.trainable_variables))
-
-            except Exception as e:
-                print("Exception occurred:", str(e))
-
-        print("Epoch {}".format(epoch + 1 ))
+        print(f"Epoch {epoch + 1}, Loss: {train_loss / len(dataset)}")
 
 # 감성말뭉치 & 송영숙 데이터 합치기 
 if __name__ == "__main__":
-    adam = tf.keras.optimizers.Adam(learning_rate = 3e-5, epsilon=1e-08)
+    # 감성말뭉치 & 송영숙 데이터 합치기 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model.to(device)
+    optimizer = AdamW(model.parameters(), lr=3e-5, eps=1e-08)
 
-    # tf dataset 만들기
-    # def 함수를 적용한 tf dataset을 만듭니다.
-    dataset = tf.data.Dataset.from_generator(tokenize_df, output_types = tf.int32)
+    # Dataset 만들기
+    dataset = list(tokenize_df())
 
-    # batch에서 가장 긴 문장을 기준으로 zero-padding을 진행합니다.
-    dataset = dataset.padded_batch(batch_size = batch_size, padded_shapes=(None,),
-                                padding_values = tokenizer.pad_token_id)
-    
-    fit_model(dataset)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, collate_fn=collate_fn, shuffle=True)
 
+    fit_model(dataloader, optimizer)
+
+    # 모델을 학습 후 저장
     model_path = './kogpt_model/'
-    # save fine-tuned tokenizer & model
-    tokenizer.save_pretrained(model_path + 'song_emotion_chatbot', bos_token='</s>', eos_token='</s>', pad_token='<pad>')
-    model.save_pretrained(model_path + 'song_emotion_chatbot')
+
+    # .pth 파일로 모델 저장
+    torch.save(model.state_dict(), model_path + 'kogpt2_model.pth')
+
+    print('Model saved to kogpt2_model.pth')
