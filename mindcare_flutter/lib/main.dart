c25eb196a1c00067ee112deaa46f652b90b28327
1TAEKIM <oneteak1@gmail.com>
2024-06-12 21:54:55 +0900

    Added functionality to save and load image data for state and data management

--- mindcare_flutter/lib/main.dart
+++ mindcare_flutter/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:provider/provider.dart'; // provider 패키지 추가
 import 'routes/app_routes.dart';
 import 'presentation/widgets/auth_checker.dart';
+import 'presentation/screens/htp_drawing_page.dart'; // htp_drawing_page 추가
 
 void main() {
   initializeDateFormatting().then((_) => runApp(const MyApp()));
@@ -12,9 +14,14 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const AuthChecker(), // 초기 위젯을 AuthChecker로 설정
-      onGenerateRoute: AppRoutes.generateRoute,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DrawingProvider()), // DrawingProvider 추가
+      ],
+      child: MaterialApp(
+        home: const AuthChecker(), // 초기 위젯을 AuthChecker로 설정
+        onGenerateRoute: AppRoutes.generateRoute,
+      ),
     );
   }
 }
