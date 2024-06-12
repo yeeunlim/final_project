5129da388a4ac6573d4e4f71c587b787205c4185
1TAEKIM <oneteak1@gmail.com>
2024-06-12 15:04:31 +0900

    Added htpTestUrl constant

--- mindcare_flutter/lib/core/constants/urls.dart
+++ mindcare_flutter/lib/core/constants/urls.dart
@@ -2,6 +2,7 @@
 const String baseUrl = 'http://127.0.0.1:8000';
 const String chatbotDiaryUrl = '$baseUrl/api/chatbot_diary';
 const String userAuthUrl = '$baseUrl/api/auth';
+const String htpTestUrl = '$baseUrl/api/htp_test';
 
 // Image URLs
 class ImageUrls {
