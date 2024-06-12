77cb967ffd8fdd04a270d3c37808bec3b6a080f6
1TAEKIM <oneteak1@gmail.com>
2024-06-12 14:57:29 +0900

    Added HTP test model

--- mindcare_django/htp_test/models.py
+++ mindcare_django/htp_test/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Drawing(models.Model):
+    TYPE_CHOICES = [
+        ('house', 'House'),
+        ('tree', 'Tree'),
+        ('person', 'Person'),
+    ]
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    image_url = models.URLField()
+    result_image_url = models.URLField(null=True, blank=True)
+    result = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return f'{self.type} drawing by {self.user.username}'
