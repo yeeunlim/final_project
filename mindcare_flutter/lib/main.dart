60951ad3898930d7b43cb06e167e982f8f27b9cc
1TAEKIM <oneteak1@gmail.com>
2024-06-14 10:54:18 +0900

    Added provider

--- mindcare_flutter/lib/main.dart
+++ mindcare_flutter/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:provider/provider.dart';
 import 'package:mindcare_flutter/presentation/widgets/auth_checker.dart';
 import 'package:mindcare_flutter/routes/app_routes.dart';
+import 'package:mindcare_flutter/presentation/screens/htp_drawing_page.dart'; // DrawingProvider 경로에 맞게 임포트
 
 void main() {
   initializeDateFormatting().then((_) => runApp(const MyApp()));
@@ -12,10 +14,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: const AuthChecker(),
-      routes: getAppRoutes(), // 정적 라우트 설정을 getAppRoutes() 함수로 대체합니다.
-      onGenerateRoute: generateRoute, // 동적 라우트 설정을 generateRoute 함수로 대체합니다.
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => DrawingProvider()), // DrawingProvider 추가
+        // 다른 Providers를 추가할 수 있습니다.
+      ],
+      child: MaterialApp(
+        home: const AuthChecker(),
+        routes: getAppRoutes(), // 정적 라우트 설정을 getAppRoutes() 함수로 대체합니다.
+        onGenerateRoute: generateRoute, // 동적 라우트 설정을 generateRoute 함수로 대체합니다.
+      ),
     );
   }
 }
