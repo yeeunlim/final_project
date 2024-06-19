316912e0532d7aedc504ca3226c8b969d9ce73f2
yeeun <shanon02@naver.com>
2024-06-19 11:03:13 +0900

    csrf 수정

--- mindcare_django/mindcare_django/settings.py
+++ mindcare_django/mindcare_django/settings.py
@@ -28,6 +28,7 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://13.209.175.99",
     "http://ec2-13-209-175-99.ap-northeast-2.compute.amazonaws.com",
     "http://localhost:8000",
     "http://localhost:8080",
